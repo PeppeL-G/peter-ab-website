869a5d3ce0744ac23012b67465173984a00d9e10
Peppe L-G <peter.larsson-green@gmail.com>
2021-10-05 17:05:54 +0200

    Improved validation (no crashing if field is missing). Closes #16.

--- validators.js
+++ validators.js
@@ -5,11 +5,15 @@ exports.getValidationErrorsForProduct = function(name, description){
 	
 	const validationErrors = []
 	
-	if(name.length < MIN_NAME_LENGTH){
+	if(!name){
+		validationErrors.push("The name is missing.")
+	}else if(name.length < MIN_NAME_LENGTH){
 		validationErrors.push("The name needs to be at least "+MIN_NAME_LENGTH+" characters.")
 	}
 	
-	if(description.length < MIN_DESCRIPTION_LENGTH){
+	if(!description){
+		validationErrors.push("The description is missing.")
+	}else if(description.length < MIN_DESCRIPTION_LENGTH){
 		validationErrors.push("The description needs to be at least "+MIN_DESCRIPTION_LENGTH+" characters.")
 	}
 	
